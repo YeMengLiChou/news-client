af10855c9c8c3a7495f752df9ffdfafb15eb1a53
gleamrise <swk2460050897@outlook.com>
2023-12-26 20:20:53 +0800

    内容管理界面大致结构

--- src/views/user/ContentManagement.vue
+++ src/views/user/ContentManagement.vue
@@ -2,16 +2,17 @@
     <div class="container">
         <el-tabs type="border-card"
                  @tab-change="handleTabChange">
-            <el-tab-pane label="未发布">
+            <el-tab-pane label="收藏">
                 <el-skeleton animated
                              :rows="8"
-                             :loading="isUnReadLoading"
+                             :loading="collectLoading"
                              :throttle="500">
                     <template #default>
-                        <div v-loading="updateLoading" v-if="unreadNotifications.length > 0">
+                        <div v-loading="updateLoading"
+                             v-if="collectionNewsData.length > 0">
                             <el-scrollbar height="100%">
                                 <el-card class="card-item"
-                                         v-for="item in unreadNotifications"
+                                         v-for="item in collectionNewsData"
                                          shadow="hover"
                                          :key="item.notificationId">
                                     <template #header>
@@ -37,21 +38,61 @@
                                 </el-card>
                             </el-scrollbar>
                         </div>
-                        <el-empty v-else/>
+                        <el-empty v-else />
+                    </template>
+                </el-skeleton>
+            </el-tab-pane>
+            <el-tab-pane label="未发布">
+                <el-skeleton animated
+                             :rows="8"
+                             :loading="unpublishLoading"
+                             :throttle="500">
+                    <template #default>
+                        <div v-loading="updateLoading"
+                             v-if="unpublishNewsData.length > 0">
+                            <el-scrollbar height="100%">
+                                <el-card class="card-item"
+                                         v-for="item in unpublishNewsData"
+                                         shadow="hover"
+                                         :key="item.notificationId">
+                                    <template #header>
+                                        <el-text>
+                                            {{ common.formatTime(item.createTime) }}
+                                        </el-text>
+                                    </template>
+                                    <el-text>
+                                        {{ item.content }}
+                                    </el-text>
+                                    <template #footer>
+                                        <div class="card-footer">
+                                            <el-button type="primary"
+                                                       @click="handleReadNotification(item.notificationId)">
+                                                已读
+                                            </el-button>
+                                            <el-button type="warning"
+                                                       @click="handleDeleteNotification(item.notificationId)">
+                                                删除
+                                            </el-button>
+                                        </div>
+                                    </template>
+                                </el-card>
+                            </el-scrollbar>
+                        </div>
+                        <el-empty v-else />
                     </template>
                 </el-skeleton>
             </el-tab-pane>
             <el-tab-pane label="已发布">
                 <el-skeleton animated
                              :rows="8"
-                             :loading="isReadLoading"
+                             :loading="publishLoading"
                              :throttle="500">
                     <template #default>
                         <div v-loading="updateLoading">
                             <el-scrollbar height="100%"
                                           v-if="readNotifications.length > 0">
                                 <el-card class="card-item"
-                                         v-for="item in readNotifications"
+                                         v-for="item in collectionNewsData"
                                          shadow="hover">
                                     <template #header>
                                         <el-text>
@@ -85,21 +126,19 @@
 import { ref, computed, watch, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useUserInfoStore } from '@/stores/userInfo';
-import noticeApi from '@/api/notification'
+import newsApi from '@/api/news';
 import msg from '@/utils/message';
 import common from '@/utils/common'
 import { ElMessageBox } from 'element-plus';
 
-const READ = 1;
-const UNREAD = 0;
-
-const isReadLoading = ref(true)
-const isUnReadLoading = ref(true)
+const publishLoading = ref(true)
+const unpublishLoading = ref(true)
+const collectLoading = ref(true)
 const updateLoading = ref(false)
 
-
-const readNotifications = ref([])
-const unreadNotifications = ref([])
+const collectionNewsData = ref([])
+const unpublishNewsData = ref([])
+const publishNewsData = ref([])
 
 const store = useUserInfoStore();
 const curTabIndex = ref(0);
@@ -120,12 +159,12 @@ function fetchNotifications(index = undefined, force = false) {
     index = index ? index : curTabIndex.value;
     console.log('notice', index);
     if (index === 0) {
-        if (unreadNotifications.value.length !== 0 && !force) return
-        isUnReadLoading.value = true;
+        if (collectionNewsData.value.length !== 0 && !force) return
+        collectLoading.value = true;
         noticeApi.fetchUnreadNotice(store.$state.state.userInfo.userId)
             .then(res => {
                 // 排序
-                unreadNotifications.value = res.map((item) => {
+                collectionNewsData.value = res.map((item) => {
                     item.createTime = new Date(item.createTime).getTime();
                     item.hasRead = false;
                     return item
@@ -133,28 +172,47 @@ function fetchNotifications(index = undefined, force = false) {
                     return b.createTime - a.createTime;
                 });
 
-                isUnReadLoading.value = false
+                collectLoading.value = false
             }).catch(err => {
                 msg.error(err.message)
-                isUnReadLoading.value = false
+                collectLoading.value = false
+            })
+    } else if (index === 1) {
+        if (collectionNewsData.value.length !== 0 && !force) return
+        unpublishLoading.value = true;
+        noticeApi.fetchUnreadNotice(store.$state.state.userInfo.userId)
+            .then(res => {
+                // 排序
+                unpublishNewsData.value = res.map((item) => {
+                    item.createTime = new Date(item.createTime).getTime();
+                    item.hasRead = false;
+                    return item
+                }).sort((a, b) => {
+                    return b.createTime - a.createTime;
+                });
+
+                unpublishLoading.value = false
+            }).catch(err => {
+                msg.error(err.message)
+                unpublishLoading.value = false
             })
     } else {
-        if (readNotifications.value.length !== 0 && !force) return
-        isReadLoading.value = true;
+        if (collectionNewsData.value.length !== 0 && !force) return
+        publishLoading.value = true;
 
         noticeApi.fetchReadNotice(store.$state.state.userInfo.userId)
             .then(res => {
-                readNotifications.value = res.map((item) => {
+                collectionNewsData.value = res.map((item) => {
                     item.createTime = new Date(item.createTime).getTime();
                     item.hasRead = false;
                     return item
                 }).sort((a, b) => {
                     return b.createTime - a.createTime;
                 });;
-                isReadLoading.value = false
+                publishLoading.value = false
             }).catch(err => {
                 msg.error(err.message)
-                isReadLoading.value = false
+                publishLoading.value = false
             })
     }
 }
@@ -168,7 +226,7 @@ function handleReadNotification(noticeId) {
     updateLoading.value = true;
     noticeApi.updateNoticeRead(noticeId).then(res => {
         updateLoading.value = false;
-        unreadNotifications.value = unreadNotifications.value.filter((item) => {
+        unpublishNewsData.value = unpublishNewsData.value.filter((item) => {
             return item.notificationId === noticeId
         })
         fetchNotifications(1, true)
@@ -177,7 +235,7 @@ function handleReadNotification(noticeId) {
     })
 }
 
-function handleDeleteNotification(noticeId, ) {
+function handleDeleteNotification(noticeId,) {
     updateLoading.value = true;
     ElMessageBox.confirm(
         '你确定要删除该条通知么？该操作不可撤回!'
@@ -185,11 +243,11 @@ function handleDeleteNotification(noticeId, ) {
         noticeApi.deleteNotice(noticeId)
             .then(res => {
                 updateLoading.value = false;
-                
-                unreadNotifications.value = unreadNotifications.value.filter((item) => {
+
+                unpublishNewsData.value = unpublishNewsData.value.filter((item) => {
                     return item.notificationId === noticeId
                 })
-                readNotifications.value = readNotifications.value.filter((item) => {
+                collectionNewsData.value = collectionNewsData.value.filter((item) => {
                     return item.notificationId === noticeId
                 })
             })
